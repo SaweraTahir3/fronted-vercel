242635950d32385914cc80b9a3ca6e63152941e0
Sawera Tahir <141661220+SaweraTahir3@users.noreply.github.com>
2024-08-11 06:45:46 +0500

    Update App.js

--- Vercel/fronted/src/App.js
+++ Vercel/fronted/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './App.css';
+ import WelcomePage from './Components/WelcomePage'
+ import StudentCourses from './Components/Result';
+ import QuizQuizQuestion from './Components/QuizQuestion'
 function App() {
   return (
     <div>
